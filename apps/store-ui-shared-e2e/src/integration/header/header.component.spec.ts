009f8d71fdc72d6c89dd63b5a91b30dc4202818f
Ryan Brady <ryan.brady@bti360.com>
2020-11-17 17:58:08 -0500

    Lab11

--- apps/store-ui-shared-e2e/src/integration/header/header.component.spec.ts
+++ apps/store-ui-shared-e2e/src/integration/header/header.component.spec.ts
@@ -4,4 +4,11 @@ describe('store-ui-shared', () => {
   it('should render the component', () => {
     cy.get('bg-hoard-header').should('exist');
   });
+
+  it('should show the title', () => {
+    cy.visit(
+      '/iframe.html?id=headercomponent--primary&knob-title=BoardGameHoard'
+    );
+    cy.get('bg-hoard-header').contains('BoardGameHoard');
+  });
 });
