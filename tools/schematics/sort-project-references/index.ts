a924e00d67d164b834d4c43626a8693dd19ab4e4
Ryan Brady <ryan.brady@bti360.com>
2020-11-18 14:25:54 -0500

    Lab14-Bonus2

--- tools/schematics/sort-project-references/index.ts
+++ tools/schematics/sort-project-references/index.ts
@@ -2,6 +2,7 @@ import { chain, Rule } from '@angular-devkit/schematics';
 import { updateJsonInTree } from '@nrwl/workspace';
 import { formatFiles } from '@nrwl/workspace';
 import { get, set } from 'lodash';
+import { join } from 'path';
 
 function sortKeysAtJsonPath(path: string, jsonPath: string[]): Rule {
   return updateJsonInTree(path, (json) => {
@@ -15,11 +16,24 @@ function sortKeysAtJsonPath(path: string, jsonPath: string[]): Rule {
   });
 }
 
+function sortRootJest(): Rule {
+  return (tree) => {
+    const jestPath = 'jest.config.js';
+    const contents = require(join(process.cwd(), jestPath));
+    contents.projects.sort();
+    tree.overwrite(jestPath, `
+      module.exports = ${JSON.stringify(contents)};
+    `);
+    return tree;
+  }
+}
+
 export default function (schema: any): Rule {
   return chain([
     sortKeysAtJsonPath('workspace.json', ['projects']),
     sortKeysAtJsonPath('nx.json', ['projects']),
     sortKeysAtJsonPath('tsconfig.base.json', ['compilerOptions', 'paths']),
+    sortRootJest(),
     formatFiles()
   ]);
 }
