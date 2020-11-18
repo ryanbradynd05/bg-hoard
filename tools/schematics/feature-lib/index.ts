74ad8c8377bb7fa758af08db9eb2475d635791d6
Ryan Brady <ryan.brady@bti360.com>
2020-11-18 13:15:07 -0500

    End lab13

--- tools/schematics/feature-lib/index.ts
+++ tools/schematics/feature-lib/index.ts
@@ -1,18 +1,18 @@
 import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
 
-interface UtilLibSchema {
+interface FeatureLibSchema {
   name: string;
   directory: string;
 }
 
-export default function (schema: UtilLibSchema): Rule {
+export default function (schema: FeatureLibSchema): Rule {
   console.log('name', schema.name);
   return chain([
     externalSchematic('@nrwl/workspace', 'lib', {
-      name: `util-${schema.name}`,
+      name: `feature-${schema.name}`,
       linter: 'tslint',
       directory: schema.directory,
-      tags: `type:util, scope:${schema.directory}`
+      tags: `type:feature, scope:${schema.directory}`
     }),
   ]);
 }
